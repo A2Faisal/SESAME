d9e5054d1768d8c80e448dccfab26ce213f487ab
Max Kaye <mkaye@Maxs-MacBook-Pro.local>
2025-10-27 10:55:11 -0400

    forced version numbers to be equal rather than >=

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sesame-iesd",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     description="A simple Python package for Sesame toolbox",
@@ -10,13 +10,13 @@ setup(
     author_email="abdullah.al.faisal@mail.mcgill.ca, maxwell.kaye@mail.mcgill.ca",
     license="MIT",
     install_requires=[
-		"geopandas>=1.0.1",
-		"xarray>=2024.6.0",
-		"h5netcdf>=1.3.0",
-		"rasterio>=1.3.10",
-		"matplotlib>=3.9.1",
-		"seaborn>=0.13.2",
-		"scipy>=1.14.0",
+		"geopandas==1.0.1",
+		"xarray==2024.6.0",
+		"h5netcdf==1.3.0",
+		"rasterio==1.3.10",
+		"matplotlib==3.9.1",
+		"seaborn==0.13.2",
+		"scipy==1.14.0",
 		"cartopy==0.23.0"
 	],
     extras_require={
