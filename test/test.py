3d359e739facc678118a188e949304df07a9ceb4
Max Kaye <mkaye@Maxs-MacBook-Pro.local>
2026-01-07 18:01:28 -0500

    updated the simple test.py file to use the new arguments

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ long_name = 'Total Railway Length in km'
 source = 'Global Railways (WFP-World Food Programme SDI-T - Logistics Database)'
 output_directory = os.path.join("data/")
 output_filename = "line_2_grid"
-ds = st.line_2_grid(lines_gdf=None, variable_name=variable_name, long_name=long_name, units="meter/grid-cell", source=source, time=None, 
-                 cell_size=1, fold_field=None, fold_function="sum", attr_field=None, shapefile_path=shapefile_path, 
-                 output_directory=output_directory, output_filename=output_filename, value_per_area=None, zero_is_value=False, verbose="yes")
+ds = st.line_2_grid(line_data=shapefile_path, variable_name=variable_name, long_name=long_name, units="meter/grid-cell", source=source, time=None, 
+                 resolution=1, agg_column=None, agg_function="sum", attr_field=None, 
+                 output_directory=output_directory, output_filename=output_filename, normalize_by_area=False, zero_is_value=False, verbose="yes")
 print(ds)
